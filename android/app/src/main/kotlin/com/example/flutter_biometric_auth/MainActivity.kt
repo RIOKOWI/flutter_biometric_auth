ba89a4463c93a0cfedf3470aae07a582b428f01c
RIOKOWI <achyarrio2004@gmail.com>
2026-05-08 14:18:07 +0700

    build(config): change config fragment

--- android/app/src/main/kotlin/com/example/flutter_biometric_auth/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_biometric_auth/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_biometric_auth
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
